88533d11f1e086967a61a666b23168ab2f7c5721
Ikennaf1 <iiikbros202@gmail.com>
2025-03-29 23:52:12 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+var navBtn = document.querySelector('#nav-open');
+var menu = document.querySelector('#nav-opened');
+var closeNav = document.querySelector('#close_nav');
+navBtn.onclick = () => {
+  menu.hidden = !menu.hidden;
+  navBtn.ariaHidden = !navBtn.ariaHidden;
+  navBtn.ariaExpanded = !navBtn.ariaExpanded;
+}
+
+closeNav.onclick = () => {
+    menu.hidden = !menu.hidden;
+    navBtn.ariaHidden = !navBtn.ariaHidden;
+    navBtn.ariaExpanded = !navBtn.ariaExpanded;
+}
