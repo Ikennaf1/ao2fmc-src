b629df571f15948529c79336a75129cc09f573fb
Ikennaf1 <iiikbros202@gmail.com>
2025-04-07 17:08:56 +0100

    Adds features page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig ({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                features: resolve(__dirname, 'features/index.html'),
+                // about: resolve(__dirname, 'about/index.html'),
+                // services: resolve(__dirname, 'services/index.html'),
+                // contact: resolve(__dirname, 'contact/index.html'),
+            },
+        },
+    },
+    plugins: [
+        tailwindcss(),
+    ],
 })
